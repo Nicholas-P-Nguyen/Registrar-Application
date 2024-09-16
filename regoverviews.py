82cc791bc5af61688d632b651ef2ec7637c27e2f
Nicholas <nguyen.p.nicholas@gmail.com>
2024-09-16 15:58:58 -0400

    Created helper function to print table

--- regoverviews.py
+++ regoverviews.py
@@ -9,6 +9,7 @@ def main():
     try:
         with sqlite3.connect(DATABASE_URL, isolation_level=None, uri=True) as connection:
             with contextlib.closing(connection.cursor()) as cursor:
+                # Help menu
                 parser = argparse.ArgumentParser(description='Registrar application: show overviews of classes')
                 parser.add_argument('-d', type=str, metavar='dept', help='show only those classes whose department contains dept')
                 parser.add_argument('-n', type=str, metavar='num', help='show only those classes whose course number contains num')
@@ -16,75 +17,51 @@ def main():
                 parser.add_argument('-t', type=str, metavar='title', help='show only those classes whose course title contains title')
                 args = parser.parse_args()
 
+                # Header
+                print(f"{'ClssId':<6} {'Dept':<4} {'CrsNum':<6} {'Area':<4} {'Title':<5}")
+                print(f"{'-' * 6} {'-' * 4} {'-' * 6} {'-' * 4} {'-' * 5}")
+
+                stmt_str = "SELECT classid, dept, coursenum, area, title "
+                stmt_str += "FROM classes, crosslistings, courses "
+                stmt_str += "WHERE courses.courseid = classes.courseid AND courses.courseid = crosslistings.courseid "
+
                 if args.d:
-                    stmt_str = "SELECT classid, dept, coursenum, area, title "
-                    stmt_str += "FROM classes, crosslistings, courses "
-                    stmt_str += "WHERE dept = ? AND courses.courseid = classes.courseid AND courses.courseid = crosslistings.courseid"
+                    stmt_str += "AND dept = ? "
+                    stmt_str += "ORDER BY dept ASC, coursenum ASC"
                     cursor.execute(stmt_str, [args.d])
-
                     table = cursor.fetchall()
-                    print(f"{'ClssId':<6} {'Dept':<4} {'CrsNum':<6} {'Area':<4} {'Title':<5}")
-                    print(f"{'-' * 6} {'-' * 4} {'-' * 6} {'-' * 4} {'-' * 5}")
-                    for row in table:
-                        line = f"{row[0]:>6} {row[1]:>4} {row[2]:>6} {row[3]:>4} {row[4]:<49}"
-                        while len(line) > 73:
-                            break_index = line.rfind(' ', 0, 74)
-                            print(line[:break_index])
-                            line = ' ' * 24 + line[break_index + 1:]
-                        print(line)
-
+                    printtable(table)
                 elif args.n:
-                    stmt_str = "SELECT classid, dept, coursenum, area, title "
-                    stmt_str += "FROM classes, crosslistings, courses "
-                    stmt_str += "WHERE coursenum LIKE ? AND courses.courseid = classes.courseid AND courses.courseid = crosslistings.courseid"
+                    stmt_str += "AND coursenum LIKE ? "
+                    stmt_str += "ORDER BY dept ASC, coursenum ASC"
                     cursor.execute(stmt_str, [args.n + '%'])
-
                     table = cursor.fetchall()
-                    print('ClsId Dept CrsNum Area Title')
-                    print('----- ---- ------ ---- -----')
-                    for i, row in enumerate(table):
-                        print(str(i), str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
+                    printtable(table)
                 elif args.a:
-                    stmt_str = "SELECT classid, dept, coursenum, area, title "
-                    stmt_str += "FROM classes, crosslistings, courses "
-                    stmt_str += "WHERE area LIKE ? AND courses.courseid = classes.courseid AND courses.courseid = crosslistings.courseid"
+                    stmt_str += "AND area LIKE ? "
+                    stmt_str += "ORDER BY dept ASC, coursenum ASC"
                     cursor.execute(stmt_str, [args.a + '%'])
-
                     table = cursor.fetchall()
-                    print('ClsId Dept CrsNum Area Title')
-                    print('----- ---- ------ ---- -----')
-                    for i, row in enumerate(table):
-                        print(str(i), str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
+                    printtable(table)
                 elif args.t:
-                    stmt_str = "SELECT classid, dept, coursenum, area, title "
-                    stmt_str += "FROM classes, crosslistings, courses "
-                    stmt_str += "WHERE title LIKE ? AND courses.courseid = classes.courseid AND courses.courseid = crosslistings.courseid"
+                    stmt_str += "AND title LIKE ? "
+                    stmt_str += "ORDER BY dept ASC, coursenum ASC"
                     cursor.execute(stmt_str, ['%' + args.t + '%'])
-
                     table = cursor.fetchall()
-                    print('ClsId Dept CrsNum Area Title')
-                    print('----- ---- ------ ---- -----')
-                    for i, row in enumerate(table):
-                        print(str(i), str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
-                else:
-                    stmt_str = "SELECT classid, dept, coursenum, area, title "
-                    stmt_str += "FROM classes, crosslistings, courses "
-                    stmt_str += "WHERE courses.courseid = classes.courseid AND courses.courseid = crosslistings.courseid"
-                    cursor.execute(stmt_str)
-
-                    table = cursor.fetchall()
-                    print('ClsId Dept CrsNum Area Title')
-                    print('----- ---- ------ ---- -----')
-                    for i, row in enumerate(table):
-                        print(str(i), str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
-
-
-
+                    printtable(table)
 
     except Exception as ex:
         print(ex, file=sys.stderr)
         sys.exit(1)
 
+def printtable(table):
+    for row in table:
+        line = f"{row[0]:>6} {row[1]:>4} {row[2]:>6} {row[3]:>4} {row[4]:<49}"
+        while len(line) > 73:
+            break_index = line.rfind(' ', 0, 74)
+            print(line[:break_index])
+            line = ' ' * 24 + line[break_index + 1:]
+        print(line)
 
 if __name__ == '__main__':
     main()
