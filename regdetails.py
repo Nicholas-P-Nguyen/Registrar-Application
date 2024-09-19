c08a106596f6972b0dd12e3595acc8ea0a48fe92
Nicholas <nguyen.p.nicholas@gmail.com>
2024-09-19 19:51:57 -0400

    Course details working

--- regdetails.py
+++ regdetails.py
@@ -2,6 +2,7 @@ import contextlib
 import sqlite3
 import argparse
 import sys
+import textwrap
 
 DATABASE_URL = 'file:reg.sqlite?mode=rw'
 
@@ -14,30 +15,66 @@ def main():
                 parser.add_argument('classid', type=str, help='the id of the class whose details should be shown')
                 args = parser.parse_args()
 
-                print('-------------')
-                print('Class Details')
-                print('-------------')
-
-                stmt_str = "SELECT classid, days, starttime, endtime, bldg, roomnum "
-                stmt_str += "FROM classes WHERE classid = ?"
-
-                cursor.execute(stmt_str, [args.classid])
-                table = cursor.fetchall()
-
-                for row in table:
-                    print('Class Id:', row[0])
-                    print('Days:', row[1])
-                    print('Start time:', row[2])
-                    print('End time:', row[3])
-                    print('Building:', row[4])
-                    print('Room:', row[5])
-
-
-
+                getClassDetails(args.classid, cursor)
+                getCourseDetails(args.classid, cursor)
 
     except Exception as ex:
         print(ex, file=sys.stderr)
         sys.exit(1)
+
+def getClassDetails(classId, cursor):
+    print('-------------')
+    print('Class Details')
+    print('-------------')
+
+    stmt_str = "SELECT classid, days, starttime, endtime, bldg, roomnum "
+    stmt_str += "FROM classes WHERE classid = ?"
+
+    cursor.execute(stmt_str, [classId])
+    table = cursor.fetchall()
+
+    for row in table:
+        print('Class Id:', row[0])
+        print('Days:', row[1])
+        print('Start time:', row[2])
+        print('End time:', row[3])
+        print('Building:', row[4])
+        print('Room:', row[5])
+
+def getCourseDetails(classId, cursor):
+    print('-------------')
+    print('Course Details')
+    print('-------------')
+
+    stmt_str = "SELECT classes.courseid, dept, coursenum, area, title, descrip, prereqs, profname "
+    stmt_str += "FROM courses, crosslistings, profs, coursesprofs, classes "
+    stmt_str += "WHERE classid = ? AND classes.courseid = courses.courseid "
+    stmt_str += "AND classes.courseid = crosslistings.courseid AND classes.courseid = coursesprofs.courseid "
+    stmt_str += "AND coursesprofs.profid = profs.profid "
+    stmt_str += "ORDER BY dept ASC, coursenum ASC"
+
+    cursor.execute(stmt_str, [classId])
+    table = cursor.fetchall()
+
+    details = ['Course Id:', 'Department and Number:', 'Area:', 'Title:', 'Description:', 'Prerequisites:' 'Professor:']
+
+    for row in table:
+        print('Course Id:', row[0])
+        print(f'Department and Number: {row[1]} {row[2]}')
+        print('Area:', row[3])
+        print('Title:', row[4])
+        printDetails('Description:' + row[5])
+        print('Prerequisites:', row[6])
+        print('Professor:', row[7])
+
+def printDetails(description):
+    while len(description) > 73:
+        break_index = description.rfind(' ', 0, 74)
+        print(description[:break_index])
+        description = ' ' * 3 + description[break_index + 1:]
+    print(description)
+
+
 
 if __name__ == '__main__':
     main()
